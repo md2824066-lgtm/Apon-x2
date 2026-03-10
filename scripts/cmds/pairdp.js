5d710c661a5a11a25618a467cd63a3575973d99e
md2824066-lgtm <md2824066@gmail.com>
2026-03-10 14:07:45 +0600

    Update pairdp.js

--- scripts/cmds/pairdp.js
+++ scripts/cmds/pairdp.js
@@ -66,7 +66,7 @@ module.exports = {
 
     } catch (err) {
       console.error("Full error:", err.response?.data || err.message);
-      return message.reply("🥹error, contact MahMUD");
+      return message.reply("🥹error, contact Apon");
     }
   }
 };
